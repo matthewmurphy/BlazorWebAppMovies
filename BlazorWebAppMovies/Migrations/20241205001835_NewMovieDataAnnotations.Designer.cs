42a3286df895966541de294f19fbd3defb22b457
matthewmurphy <matthew@the-murphys.net>
2024-12-05 10:03:24 -0700

    Step 7: Add new field to movie (Rating)

--- BlazorWebAppMovies/Migrations/20241205001835_NewMovieDataAnnotations.Designer.cs
+++ BlazorWebAppMovies/Migrations/20241205001835_NewMovieDataAnnotations.Designer.cs
@@ -4,6 +4,7 @@ using BlazorWebAppMovies.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorWebAppMovies.Migrations
 {
     [DbContext(typeof(BlazorWebAppMoviesContext))]
-    partial class BlazorWebAppMoviesContextModelSnapshot : ModelSnapshot
+    [Migration("20241205001835_NewMovieDataAnnotations")]
+    partial class NewMovieDataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -37,10 +40,6 @@ namespace BlazorWebAppMovies.Migrations
 
                     b.Property<decimal>("Price")
                         .HasColumnType("decimal(18, 2)");
-
-                    b.Property<string>("Rating")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
 
                     b.Property<DateOnly>("ReleaseDate")
                         .HasColumnType("date");
